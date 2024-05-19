2625597f99a3907aaefd85ef7743a7ac5c920b6c
MatheusVelame <mvp2@cesar.school>
2024-05-19 00:04:39 -0300

    adaptando para clion

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ void free_maze(int linhas, char **maze) {
         free(maze[i]);
     }
 
-    free(maze);   
+    free(maze);
 }
 
 void read_maze(int *linhas, int *colunas, char ***maze) {
@@ -26,11 +26,11 @@ void read_maze(int *linhas, int *colunas, char ***maze) {
     }
 
     fscanf(f, "%d %d ", linhas, colunas);
-    *maze = (char **)calloc(*linhas, sizeof(char *));  
+    *maze = (char **)calloc(*linhas, sizeof(char *));
     for (int i = 0; i < *linhas; i++)
     {
         (*maze)[i] = (char *)calloc(*colunas + 1, sizeof(char));
-    
+
         fgets(tmp, 13, f);
         if (tmp[strlen(tmp) - 1] == '\n')
         {
@@ -39,21 +39,21 @@ void read_maze(int *linhas, int *colunas, char ***maze) {
 
         strcpy((*maze)[i], tmp);
     }
-    
-    fclose(f);   
+
+    fclose(f);
 }
 
 void draw_maze(int linhas, int colunas, char **maze) {
-    int scale_x = 7;  
+    int scale_x = 7;
     int scale_y = 3;
 
     int startX = MINX + 1;
     int startY = MINY + 1;
 
     for (int y = 0; y < linhas; y++) {
-        for (int sy = 0; sy < scale_y; sy++) { 
+        for (int sy = 0; sy < scale_y; sy++) {
             for (int x = 0; x < colunas; x++) {
-                for (int sx = 0; sx < scale_x; sx++) {  
+                for (int sx = 0; sx < scale_x; sx++) {
                     screenGotoxy(startX + x * scale_x + sx, startY + y * scale_y + sy);
                     if ((maze)[y][x] == '1')
                     {
@@ -63,14 +63,14 @@ void draw_maze(int linhas, int colunas, char **maze) {
 
                         printf(" ");
                     }
-                    
+
                 }
             }
         }
     }
 }
 
-int main() 
+int main()
 {
     int linhas;
     int colunas;
