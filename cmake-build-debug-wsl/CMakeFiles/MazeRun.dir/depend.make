e9955e6f77aa32b18ee03cc2935752be8ae623f7
MatheusVelame <mvp2@cesar.school>
2024-05-19 22:49:14 -0300

    ajustes na alocação da matriz

--- cmake-build-debug-wsl/CMakeFiles/MazeRun.dir/depend.make
+++ cmake-build-debug-wsl/CMakeFiles/MazeRun.dir/depend.make
@@ -4,9 +4,7 @@
 CMakeFiles/MazeRun.dir/src/keyboard.c.o: ../include/keyboard.h
 CMakeFiles/MazeRun.dir/src/keyboard.c.o: ../src/keyboard.c
 
-CMakeFiles/MazeRun.dir/src/main.c.o: ../include/keyboard.h
 CMakeFiles/MazeRun.dir/src/main.c.o: ../include/screen.h
-CMakeFiles/MazeRun.dir/src/main.c.o: ../include/timer.h
 CMakeFiles/MazeRun.dir/src/main.c.o: ../src/main.c
 
 CMakeFiles/MazeRun.dir/src/screen.c.o: ../include/screen.h
--- src/main.c
+++ src/main.c
@@ -1,65 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "screen.h"
 
 // Define o tamanho do labirinto
-#define coluna 20
-#define linha 20
+#define COLUNA 20
+#define LINHA 19
 
-// Define o labirinto como uma matriz de caracteres
-char labirinto_inicial[linha][coluna + 1] = {
-    "1E111111111111111111",
-    "1  111111          1",
-    "11 11   11 1111 11 1",
-    "11    1    1     1 1",
-    "1111111111 1 11  1 1",
-    "1   1   11 1 11    1",
-    "11  1 1 11      11 1",
-    "1     1 11111 1 1 11",
-    "1 11 11       1  1 1",
-    "1  1 11111111111   1",
-    "11 1 11   11    11 1",
-    "1  1 11 1    11  1 1",
-    "1  1    1111 111 111",
-    "11   1111    111 111",
-    "11 1     1111    111",
-    "1   111  1    111111",
-    "1 1   1 1  11 111111",
-    "1   1 1 1 1        1",
-    "111111111S1111111111"
-};
-
-//aloca dinamicamente o labirinto
-void aloca_labirinto(char ***labirinto) {
-
-    *labirinto = (char **)calloc(linha, sizeof(char *));
-
-    for (int i = 0; i < linha; i++)
-    {
-        (*labirinto)[i] = (char *)calloc((coluna + 1), sizeof(char));
-    }
-}
-
-void preenche_labirinto(char **labirinto) {
-
-    for (int i = 0; i < linha; i++)
-    {
-        for (int j = 0; j < coluna + 1; j++)
-        {
-            labirinto[i][j] = labirinto_inicial[i][j];
-        }
-    }   
-}
 // Função para desenhar o labirinto na tela
 void desenhaLabirinto(char **labirinto)
 {
-    int offsetX = (MAXX - coluna) / 2;
-    int offsetY = (MAXY - linha) / 2;
+    int offsetX = (MAXX - COLUNA) / 2;
+    int offsetY = (MAXY - LINHA) / 2;
 
-    for (int y = 0; y < linha; y++)
+    for (int y = 0; y < LINHA; y++)
     {
         screenGotoxy(offsetX + 1, offsetY + y + 1); // Define a posição do cursor com o deslocamento calculado
-        for (int x = 0; x < coluna; x++)
+        for (int x = 0; x < COLUNA; x++)
         {
             char ch = labirinto[y][x];
             if (ch == '1')
@@ -86,17 +43,59 @@ void desenhaLabirinto(char **labirinto)
 
 int main()
 {
+    int i;
     char **labirinto;
+
+    // Alocação dinâmica da matriz labirinto
+    labirinto = (char **)calloc(LINHA, sizeof(char*));
+    for (i = 0; i < LINHA; i++)
+    {
+        labirinto[i] = (char *)calloc(COLUNA + 1, sizeof(char));
+    }
+
+    // Preenchendo o labirinto com o conteúdo desejado
+    char labirintoInicial[LINHA][COLUNA + 1] = {
+        "1E111111111111111111",
+        "1  111111          1",
+        "11 11   11 1111 11 1",
+        "11    1    1     1 1",
+        "1111111111 1 11  1 1",
+        "1   1   11 1 11    1",
+        "11  1 1 11      11 1",
+        "1     1 11111 1 1 11",
+        "1 11 11       1  1 1",
+        "1  1 11111111111   1",
+        "11 1 11   11    11 1",
+        "1  1 11 1    11  1 1",
+        "1  1    1111 111 111",
+        "11   1111    111 111",
+        "11 1     1111    111",
+        "1   111  1    111111",
+        "1 1   1 1  11 111111",
+        "1   1 1 1 1        1",
+        "111111111S1111111111"
+    };
+
+    for (i = 0; i < LINHA; i++) // Copia a matriz estática para a matriz dinámica
+    {
+        strcpy(labirinto[i], labirintoInicial[i]);
+    }
+
     // Inicializa a tela e desenha as bordas
     screenInit(1);
-    aloca_labirinto(&labirinto);
-    preenche_labirinto(labirinto);
 
     // Desenha o labirinto na tela
     desenhaLabirinto(labirinto);
 
     // Aguarda o usuário pressionar Enter para finalizar
     getchar();
+
+    // Libera a memória alocada
+    for (i = 0; i < LINHA; i++)
+    {
+        free(labirinto[i]);
+    }
+    free(labirinto);
 
     // Finaliza a tela
     screenDestroy();
